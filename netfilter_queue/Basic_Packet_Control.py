769d4587762c28db6bea7cd35ab31e84b08d6164
Brandt Woolf <bwoolf16@gmail.com>
2016-05-03 13:07:23 -0400

    Script to control outgoing packets on server side

---
+++ netfilter_queue/Basic_Packet_Control.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from netfilterqueue import NetfilterQueue
+
+def print_and_accept(pkt):
+    print pkt
+	raw_input("Press Enter to send packet\n")
+    pkt.accept()
+
+nfqueue = NetfilterQueue()
+nfqueue.bind(1, print_and_accept)
+try:
+    nfqueue.run()
+except KeyboardInterrupt:
+    print
